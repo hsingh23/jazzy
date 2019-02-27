512288ede3137b5b3838723b921cf85b62fa70e0
Harsh Singh 🔥 💯 🦄 <harsh.singh@tamr.com>
2019-02-27 02:38:11 -0500

    blog

--- utils/config.js
+++ utils/config.js
@@ -1,76 +1,92 @@
 module.exports = {
-  baseUrl: "http://next-blog-firestore.now.sh",
+  baseUrl: 'https://jazzy.roomtolearn.club',
   siteTitle: "Jazzy's Q and A",
-  description: "Jasmine gets your questions and answers them",
-  copyright: "MIT.",
+  description: 'Jasmine gets your questions and answers them',
+  copyright: 'MIT.',
   languages: {
     en: {
-      id: "en",
-      title: "English",
-      shortTitle: "Eng"
-    }
+      id: 'en',
+      title: 'English',
+      shortTitle: 'Eng',
+    },
   },
-  defaultLanguage: "en",
-  disqusShortname: "",
+  defaultLanguage: 'en',
+  disqusShortname: '',
   menu: [
     {
-      text: "Home",
-      route: "index"
+      text: 'Home',
+      route: 'index',
     },
     {
-      text: "Tutorials",
-      route: "category",
+      text: 'Tutorials',
+      route: 'category',
       params: {
-        category: "tutorials"
-      }
-    }
+        category: 'tutorials',
+      },
+    },
   ],
   social: {
     facebook: {
-      href: "https://facebook.com",
-      title: "Facebook"
+      href: 'https://facebook.com',
+      title: 'Facebook',
     },
     instagram: {
-      href: "https://www.instagram.com",
-      title: "Instagram"
+      href: 'https://www.instagram.com',
+      title: 'Instagram',
     },
     vimeo: {
-      href: "https://vimeo.com",
-      title: "Vimeo"
-    }
+      href: 'https://vimeo.com',
+      title: 'Vimeo',
+    },
   },
   authors: {
-    jazzy: {
-      id: "jazzy",
-      name: "Jasmine",
-      description: "The one and only",
-      avatar: "/assets/authors/john.svg",
+    john: {
+      id: 'john',
+      name: 'John',
+      description: 'The one and only',
+      avatar: '/assets/authors/john.svg',
       social: {
         facebook: {
-          href: "https://www.facebook.com",
-          title: "Facebook"
+          href: 'https://www.facebook.com',
+          title: 'Facebook',
         },
         instagram: {
-          href: "https://www.instagram.com/",
-          title: "Instagram"
-        }
-      }
+          href: 'https://www.instagram.com/',
+          title: 'Instagram',
+        },
+      },
+    },
+    jazzy: {
+      id: 'jazzy',
+      name: 'Jasmine',
+      description: 'The one and only',
+      avatar: '/assets/authors/john.svg',
+      social: {
+        facebook: {
+          href: 'https://www.facebook.com',
+          title: 'Facebook',
+        },
+        instagram: {
+          href: 'https://www.instagram.com/',
+          title: 'Instagram',
+        },
+      },
     },
     harsh: {
-      id: "harsh",
-      name: "Harsh",
-      description: "Wildcard",
-      avatar: "/assets/authors/john.svg",
+      id: 'harsh',
+      name: 'Harsh',
+      description: 'Wildcard',
+      avatar: '/assets/authors/john.svg',
       social: {
         facebook: {
-          href: "https://www.facebook.com",
-          title: "Facebook"
+          href: 'https://www.facebook.com',
+          title: 'Facebook',
         },
         instagram: {
-          href: "https://www.instagram.com/",
-          title: "Instagram"
-        }
-      }
-    }
-  }
+          href: 'https://www.instagram.com/',
+          title: 'Instagram',
+        },
+      },
+    },
+  },
 };
